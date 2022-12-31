0ad46c07aaf349a9f3b867ba70ac72f356e83a98
mutian <bhfqlz@qq.com>
2022-12-31 20:27:55 +0800

    notices:
    version 2.0.11

--- src/config/ExecutorDefaultConfig.php
+++ src/config/ExecutorDefaultConfig.php
@@ -5,7 +5,7 @@ namespace muyomu\executor\config;
 use muyomu\config\annotation\Configuration;
 use muyomu\config\GenericConfig;
 
-#[Configuration("executor_config")]
+#[Configuration("config_executor")]
 class ExecutorDefaultConfig extends GenericConfig
 {
     protected string $configClass = self::class;
